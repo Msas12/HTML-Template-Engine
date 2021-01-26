5a61b3e93e6d6b60fd9c73bd92c59bd13bafcb83
Mike Sasiela <m5455y@gmail.com>
2021-01-25 21:01:29 -0600

    worked on employee class and got main portion of app.js set up to run when other classes are complete

--- app.js
+++ app.js
@@ -13,6 +13,81 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const promptUser = () =>
+{    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Employee Type?',
+            name: 'type',
+            choices: ['Engineer', 'Manager', 'Intern', 'Complete Team'],
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is their name?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is their employee ID?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is their email?',
+        },
+    ]).then((answers) => {
+        console.log(answers)
+        if (answer == 'Manager') {
+            manager(answers)
+        }else if (answer == 'Engineer') {
+            engineer(answers)
+        }else if (answer == 'Intern') {
+            intern(answers)
+        }else {
+            render(employees)
+        }
+      })
+} 
+const  manager = (a) => 
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is their Office Number?',
+        },
+    ]).then((answers) => {
+     const newManager =   new Manager(a.name, a.id, a.email, answers.officeNumber)
+     employees.push(newManager)
+     promptUser()
+    })
+
+const engineer = () =>
+    inquirer.prompt([
+        {
+            // type: 'input',
+            // name: 'officeNumber',
+            // message: 'What is their Office Number?',
+        },
+    ])
+
+    const intern = () =>
+    inquirer.prompt([
+        {
+            // type: 'input',
+            // name: 'officeNumber',
+            // message: 'What is their Office Number?',
+        },
+    ])
+
+    const completeTeam = () =>
+    inquirer.prompt([
+        {
+            // type: 'input',
+            // name: 'officeNumber',
+            // message: 'What is their Office Number?',
+        },
+    ])
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -24,6 +99,11 @@ const render = require("./lib/htmlRenderer");
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
+const employees = []
+
+promptUser()
+
+
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
@@ -32,4 +112,9 @@ const render = require("./lib/htmlRenderer");
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// for the provided `render` function to work! ``
+
+// => fs.writeFile(outputPath, render(answers)))
+//   .then(() => console.log('Successfully wrote to team.html and placed it in the output folder!'))
+//   .catch((err) => console.error(err));
+// employees.push(answers)
